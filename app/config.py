ecf6eea81aa56fc7d89d975753bab9bba0a264ce
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-04-10 13:07:40 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,1 @@
 URL = 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php'
-URL_login = 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php'
-URL_details = 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/api.php'
