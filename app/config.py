2c1c5d9fee3c7e695f91db553a97789385273bfc
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-04-15 15:55:17 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,2 +1,3 @@
 URL = 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php'
 URL_details = 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/api.php'
+alert_url = 'https://hooks.slack.com/services/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'
