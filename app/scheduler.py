c6948115c358b445844521c5441b1146285af1d5
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-04-22 12:05:15 +0530

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -7,39 +7,50 @@ import numpy as np
 
 
 
+
 def overall_reviewes():
-    users = mongo.db.reports.find_one({"cron_checkin": False}, {'user': 1,'_id':0})
-    id = users['user']
-    
-    docs = mongo.db.reports.update_one({
+    users = mongo.db.reports.find({"cron_checkin":True})
+    users = [serialize_doc(doc) for doc in users]
+    for detail in users:
+        id=detail['user']
+        print(id)
+        docs = mongo.db.reports.update({
+        "user":str(id)
+        }, {
+        "$set": {
+           "cron_checkin": False
+           }}, upsert=False)
+        docs = mongo.db.reports.find({"user":str(id)})
+        user = mongo.db.users.find_one({"_id":ObjectId(id)})
+        weights = user['managers']
+        all_weight=[]
+        for weg in weights:
+            weight = weg['weight']
+            all_weight.append(weight)
+        docs = [serialize_doc(doc) for doc in docs]
+        all_sum=[]
+        for detail in docs:
+            review=detail['review']
+            rating=review['rating']
+            all_sum.append(rating)
+        weighted_avg = np.average(all_sum, weights=all_weight)
+        ret = mongo.db.users.update({
+                "_id": ObjectId(id)
+            }, {
+                "$set": {
+                    "Overall_rating":weighted_avg
+                }
+                })            
+  
+
+
+def update_croncheckin():
+    docs = mongo.db.reports.update({
        "cron_checkin": False
     }, {
        "$set": {
            "cron_checkin": True
-           }}, upsert=False)
-    
-    docs = mongo.db.reports.find({"user":str(id)})
-    user = mongo.db.users.find_one({"_id":ObjectId(id)})
-    weights = user['managers']
-    all_weight=[]
-    for weg in weights:
-       weight = weg['weight'] 
-       all_weight.append(weight) 
-    docs = [serialize_doc(doc) for doc in docs]
-    all_sum=[]
-    for detail in docs:   
-       review=detail['review']
-       rating=review['rating']
-       all_sum.append(rating)
-    weighted_avg = np.average(all_sum, weights=all_weight) 
-    ret = mongo.db.users.update({
-            "_id": ObjectId(id)
-        }, {
-            "$set": {
-                "Overall_rating":weighted_avg 
-            }
-        })
-  
+           }}, upsert=False,multi=True)
 
 
 
