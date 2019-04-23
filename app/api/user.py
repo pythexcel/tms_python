41e5cea7b68d9ff1a00d2b8f106ef28ddd7977f8
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-04-23 13:04:09 +0530

    Update user.py

--- app/api/user.py
+++ app/api/user.py
@@ -43,3 +43,85 @@ def user_assign_role(user_id, role):
         return jsonify(ret), 200
     else:
         return jsonify(msg="invalid role"), 500
+
+    
+    
+   
+@bp.route("/recent_activity", methods=["GET"])
+@jwt_required
+def recent_activity():
+        today = datetime.date.today()
+        last_sunday = today - datetime.timedelta(days=(today.weekday() + 1))
+        last_monday = today - datetime.timedelta(days=(today.weekday() + 8))
+
+        current_user = get_current_user()
+        if current_user["role"] == "Admin" or "manager":
+            users = mongo.db.users.find({
+                "managers": {
+                    "$elemMatch": {"_id": str(current_user["_id"])}
+                }
+                })
+            user_ids = []
+            for user in users:
+                user_ids.append(str(user['_id']))
+            for data in  user_ids:
+                docs = mongo.db.reports.find({
+                    "user":str(data),
+                    "review": {'$exists': False},
+                    "created_at": {
+                    "$gte": datetime.datetime(last_monday.year, last_monday.month, last_monday.day),
+                    "$lte": datetime.datetime(last_sunday.year, last_sunday.month, last_sunday.day)
+                     }  
+                })
+                docs = [serialize_doc(doc) for doc in docs]
+                user_id = []
+                for user in docs:
+                    user_id.append(user)
+                if user_id is not None: 
+                    ret = mongo.db.recent_activity.update({
+                    "user": str(current_user["_id"]),},
+                    {"$set":{"recent_activity":{
+                    "created_at": datetime.datetime.now(),
+                    "Message":"Your have not reviewed your juniors weekly report"
+                    }}},upsert=True)
+                return jsonify(str(ret))       
+        else:
+            docs = mongo.db.reports.find({
+                "user": str(current_user["_id"]),
+                "review": {'$exists': True},  
+                "created_at": {
+                "$gte": datetime.datetime(last_monday.year, last_monday.month, last_monday.day),
+                "$lte": datetime.datetime(last_sunday.year, last_sunday.month, last_sunday.day)
+            }
+            })
+            docs = [serialize_doc(doc) for doc in docs]
+            if not docs:
+                return("review not available")
+            else:
+                ret = mongo.db.recent_activity.update({
+                    "user": str(current_user["_id"]),},
+                    {"$set":{"recent_activity":{
+                    "created_at": datetime.datetime.now(),
+                    "user": str(current_user["_id"] ),
+                    "Message":"Your weekly report has been reviewed"
+                    }}},upsert=True)
+                
+            last_day = today - datetime.timedelta(1)
+            next_day = today + datetime.timedelta(1)
+
+            reports = mongo.db.reports.find({
+            "user": str(current_user["_id"]),
+            "type": "daily",
+            "created_at": {
+                "$gte": datetime.datetime(last_day.year, last_day.month, last_day.day),
+                "$lte": datetime.datetime(next_day.year, next_day.month, next_day.day)
+            }
+            })
+            reports = [serialize_doc(report) for report in reports]
+            if not reports:
+                ret = mongo.db.recent_activity.update({
+                    "user": str(current_user["_id"]),},
+                    {"$set":{"recent_activity":{
+                        "checkin_message":"You have missed your daily checkin"
+                    }}},upsert=True)
+            return jsonify(docs), 200
