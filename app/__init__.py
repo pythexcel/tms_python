36d08e272a89bc0ac997978cf44d49636dcc06b6
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-05-30 18:58:14 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -76,7 +76,7 @@ def create_app(test_config=None):
     reset_scheduler.start()
     
     recent_activity_scheduler = BackgroundScheduler()
-    recent_activity_scheduler.add_job(recent_activity, trigger='cron', day_of_week='mon-sat', hour=18, minute=00)
+    recent_activity_scheduler.add_job(recent_activity, trigger='cron', day_of_week='mon-sat', hour=17, minute=00)
     recent_activity_scheduler.start()
     
     weekly_remainder_scheduler = BackgroundScheduler()
