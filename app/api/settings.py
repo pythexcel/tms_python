1ebe8a59754eaa2d4dda7c8b2df2a4b7bc1fb848
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-06-17 19:04:48 +0530

    Update settings.py

--- app/api/settings.py
+++ app/api/settings.py
@@ -1,34 +1,41 @@
 from flask import (
-   Blueprint, g, request, abort, jsonify)
+    Blueprint, g, request, abort, jsonify)
 from app import mongo
 from flask_jwt_extended import (
-    jwt_required, create_access_token, get_current_user
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity, get_current_user, jwt_refresh_token_required,
+    verify_jwt_in_request
 )
-
+from app import token
+from bson import ObjectId
 
 bp = Blueprint('system', __name__, url_prefix='/system')
 
 
-@bp.route('/settings', methods=['POST'])
+@bp.route('/slack_settings', methods=["POST"])
+@bp.route('/slack_settings/<string:id>', methods=["PUT"])
 @jwt_required
-def settings():
- user = get_current_user()
+@token.admin_required
+def slack_setings(id):
+    webhook_url = request.json.get("webhook_url", None)
+    slack_token = request.json.get("slack_token", None)
 
- if user["role"] == "Admin":   
-    if not request.json:
-        abort(500)
-    integrate_with_hr = request.json.get('integrate_with_hr', False)
-    if integrate_with_hr is True:
-        hr = mongo.db.hr.insert_one({
-            "integrate_with_hr": integrate_with_hr
+    if request.method == "POST":
+        ret = mongo.db.slack_tokens.insert_one({
+            "webhook_url": webhook_url,
+            "slack_token": slack_token
         }).inserted_id
-        return jsonify(str(hr))
-    else:
-        hr = mongo.db.hr.update({
-            "integrate_with_hr": True
+        return jsonify(str(ret))
+
+    if request.method == "PUT":
+        ret = mongo.db.slack_tokens.update({
+            "_id": ObjectId(id)
         }, {
-                "$unset": {
-                     "integrate_with_hr": integrate_with_hr
-                 }
-             })
-        return ("settings off")
+            "$set": {
+                "webhook_url": webhook_url,
+                "slack_token": slack_token
+            }
+        }
+        )
+        return jsonify(str(ret))
+
