085d22c97554b8aaaf0fad1b1db932556bf11f17
pythexcel <46257768+pythexcel@users.noreply.github.com>
2019-06-21 14:31:14 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,32 @@
 URL = 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php'
 URL_details = 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/api.php'
-attn_url = 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php'
 secret_key = '3dd7fe8a6ea2ea9afb9a7366980253b7'
-webhook_url = 'https://hooks.slack.com/services/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'
+
+checkin_score_scheduler_seconds = 90
+
+overall_score_scheduler_hour = 16
+overall_score_scheduler_min = 30
+
+reset_cron_scheduler_hour =18
+reset_cron_scheduler_min = 10
+
+missed_checkin_scheduler_hour =11
+missed_checkin_scheduler_min =30
+
+weekly_remainder_scheduler_hour=16
+weekly_remainder_scheduler_min=45
+
+review_activity_scheduler_hour =11
+review_activity_scheduler_min =30
+
+disable_user_scheduler_hour=20
+disable_user_scheduler_min=30
+
+monthly_score_scheduler_hour=13
+monthly_score_scheduler_min=10
+
+monthly_remainder_scheduler_hour=18
+monthly_remainder_scheduler_min=5
+
+monthly_manager_reminder_scheduler_hour=17
+monthly_manager_reminder_scheduler_min=30
