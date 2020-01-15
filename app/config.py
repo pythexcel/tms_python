94dfafad54f460c2c9ae4d182a5c9c7bd5c59c3e
aayush <rasealex000000@gmail.com>
2020-01-15 18:16:20 +0530

    fix

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 URL = 'http://dynamic.hr.excellencetechnologies.in/'
 
 notification_system_url = 'http://176.9.137.77:8007/' #'http://127.0.0.1:8000/' #'http://5.9.144.22:8005/'
-tms_system_url = 'http://127.0.0.1:8000/' #server ip and port on which tms code running.
+tms_system_url = 'http://127.0.0.1:5000/' #server ip and port on which tms code running.
 
 weekly_page_link="http://tms.excellencetechnologies.in/#/app/week/WeeklyReport?update=true"
 
